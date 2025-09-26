21a1a11ea58fc32f3831a023812c3abd463270ce
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-26 16:41:06 +0500

    Add project files.

---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using System;
+
+namespace Tyuiu.GalchinskayaVV.Sprint1.Task0.V19.Lib
+{
+    public class DataService : ISprint1Task0V19
+    {
+        public double Calculate()
+        {
+            return 4.0 / 2.0 * 5.0 / (3.0 + 2.0) * 5.0;
+        }
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            var result = ds.Calculate();
+            var wait = 10.0; // 4/2=2, 2*5=10, 10/(3+2)=10/5=2, 2*5=10
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
