e492559fa302dd5be93d1d7e636022f353e6c929
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-28 21:14:19 +0500

    Task2

---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task2.V21.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using System;
+
+namespace Tyuiu.GalchinskayaVV.Sprint1.Task1.V21.Lib
+{
+    public class DataService : ISprint1Task1V21
+    {
+        public double Calculate(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint1.Task1.V21.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            double x = 5.0;
+            double y = 3.0;
+            var result = ds.Calculate(x, y);
+            var wait = 15.0; // 5 * 3 = 15
+
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithZero()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.0;
+            double y = 10.0;
+            var result = ds.Calculate(x, y);
+            var wait = 0.0; // 0 * 10 = 0
+
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCalculateWithLargeNumbers()
+        {
+            DataService ds = new DataService();
+
+            double x = 25.0;
+            double y = 40.0;
+            var result = ds.Calculate(x, y);
+            var wait = 1000.0; // 25 * 40 = 1000
+
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCalculateSquare()
+        {
+            DataService ds = new DataService();
+
+            double x = 7.0;
+            double y = 7.0;
+            var result = ds.Calculate(x, y);
+            var wait = 49.0; // 7 * 7 = 49
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
