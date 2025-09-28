321d406d8b6327e3e72183ec2dd0c4394816e43a
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-28 22:19:21 +0500

    Task2

---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task3.V7.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint1.Task3.V7.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
