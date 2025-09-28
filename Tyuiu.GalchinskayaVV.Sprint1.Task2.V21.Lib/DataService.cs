45fb553bbff4fa0d844dafba5d2537a96884bf07
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-09-28 22:39:57 +0500

    Task2

--- Tyuiu.GalchinskayaVV.Sprint1.Task2.V21.Lib/DataService.cs
+++ Tyuiu.GalchinskayaVV.Sprint1.Task2.V21.Lib/DataService.cs
@@ -3,11 +3,16 @@ using System;
 
 namespace Tyuiu.GalchinskayaVV.Sprint1.Task2.V21.Lib
 {
-    public class DataService : ISprint1Task1V21
+    public class DataService : ISprint1Task2V21
     {
         public double Calculate(double x, double y)
         {
             return x * y;
+        }
+
+        public int CalculateRectangleSquare(int value, int valueTwo)
+        {
+            throw new NotImplementedException();
         }
     }
 }
